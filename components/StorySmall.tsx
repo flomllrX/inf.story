47315a0e9bd59f6c26e8862e9630153d773de264
Victor Forissier <victor.fgds@gmail.com>
2019-12-11 09:07:48 +0000

    feature: adding dynamic picture mapping

--- components/StorySmall.tsx
+++ components/StorySmall.tsx
@@ -34,20 +34,56 @@ const styles = StyleSheet.create({
   }
 });
 
+const PORTRAITS = [
+  {
+    portrait: require("../assets/portraits/squire.png"),
+    value: "squire"
+  },
+  {
+    portrait: require("../assets/portraits/rogue.png"),
+    value: "rogue"
+  },
+  {
+    portrait: require("../assets/portraits/knight.png"),
+    value: "knight"
+  },
+  {
+    portrait: require("../assets/portraits/noble.png"),
+    value: "noble"
+  },
+  {
+    portrait: require("../assets/portraits/peasant.png"),
+    value: "peasant"
+  },
+  {
+    portrait: require("../assets/portraits/ranger.png"),
+    value: "ranger"
+  },
+  {
+    portrait: require("../assets/portraits/orc.png"),
+    value: "orc"
+  },
+  {
+    portrait: require("../assets/portraits/shadow.png"),
+    value: "shadow"
+  }
+];
+
 const StorySmall: React.SFC<Props> = ({
   createdAt,
   origin,
   uid,
   navigation
 }) => {
-  // TODO: replace with dynamic path
-  const path: string = `../assets/portraits/squire.png`;
   return (
     <TouchableOpacity
       style={styles.container}
       onPress={() => navigation.navigate("StoryModal", { storyId: uid })}
     >
-      <Image style={{ width: 70, height: 70 }} source={require(path)} />
+      <Image
+        style={{ width: 70, height: 70 }}
+        source={PORTRAITS.find(c => c.value === origin.class).portrait}
+      />
       <View>
         <Text style={styles.text}>
           {origin.name}, the {origin.class}
