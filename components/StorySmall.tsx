869b34da523385d7f88c3b9abce89aa2946b3da7
Victor Forissier <victor.fgds@gmail.com>
2019-12-11 00:26:31 +0000

    feature: adding elements to component

--- components/StorySmall.tsx
+++ components/StorySmall.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { Origin } from "../types";
 import PropTypes from "prop-types";
 import { colors, fonts } from "../theme";
@@ -9,7 +9,6 @@ import { withNavigation } from "react-navigation";
 interface Props {
   createdAt: string;
   origin: Origin;
-  title: string;
   uid: string | number;
   navigation: any;
 }
@@ -17,29 +16,46 @@ interface Props {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    padding: 20,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    flexWrap: "wrap"
   },
   text: {
     color: colors.defaultText,
-    fontFamily: fonts.regular
+    fontFamily: fonts.regular,
+    fontSize: 20
+  },
+  subText: {
+    color: colors.lightgray,
+    fontSize: 14
   }
 });
 
 const StorySmall: React.SFC<Props> = ({
   createdAt,
   origin,
-  title,
   uid,
   navigation
 }) => {
-  console.log("Nav", navigation);
+  // TODO: replace with dynamic path
+  const path: string = `../assets/portraits/squire.png`;
   return (
     <TouchableOpacity
       style={styles.container}
       onPress={() => navigation.navigate("StoryModal", { storyId: uid })}
     >
-      <Text style={styles.text}>{title}</Text>
-      <Text style={styles.text}>{new Date(createdAt).toDateString()}</Text>
+      <Image style={{ width: 70, height: 70 }} source={require(path)} />
+      <View>
+        <Text style={styles.text}>
+          {origin.name}, the {origin.class}
+        </Text>
+        <Text style={[styles.text, styles.subText]}>
+          {new Date(createdAt).toDateString()}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -49,7 +65,6 @@ StorySmall.defaultProps = {};
 StorySmall.propTypes = {
   createdAt: PropTypes.string,
   origin: PropTypes.any,
-  title: PropTypes.string,
   uid: PropTypes.any,
   navigation: PropTypes.any
 };
