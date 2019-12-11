42f3017124d9eb2594f2f0d6b21d54694f3d0290
Victor Forissier <victor.fgds@gmail.com>
2019-12-11 19:53:34 +0000

    feature: update of welcoming string: better title, and 'resume last story' component

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -11,8 +11,7 @@ import { colors, fonts } from "../theme";
 import ControlService from "../services/ControlService";
 import { inject, observer } from "mobx-react";
 import { withNavigation } from "react-navigation";
-import MainTitle from "../components/MainTitle";
-
+import PORTRAITS from "../assets/portraits/PATHS";
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.background,
@@ -41,17 +40,35 @@ const styles = StyleSheet.create({
     marginTop: 50
   },
   img: {
-    // TODO: center svg img
+    width: 390,
+    height: 120
   },
   android: {
     width: 50,
     height: 20
+  },
+  resumeView: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "90%",
+    marginLeft: "5%"
+  },
+  resumeButton: {
+    position: "absolute",
+    bottom: 50
+  },
+  bold: {
+    fontFamily: fonts.bold
   }
 });
 
 interface WelcomeState {
   playerClass: string;
   name: string;
+  location: string;
+  storyId: string;
 }
 
 class Welcome extends Component<any, WelcomeState> {
@@ -59,7 +76,9 @@ class Welcome extends Component<any, WelcomeState> {
 
   state = {
     playerClass: "noble",
-    name: ""
+    name: "",
+    location: "",
+    storyId: ""
   };
 
   onStart = () => {
@@ -76,20 +95,46 @@ class Welcome extends Component<any, WelcomeState> {
 
   render() {
     const { mainStore } = this.props;
+    let { playerClass, name, location } = this.state;
+    const { stories, storyId } = mainStore;
 
-    console.log("heeeeeeere", mainStore);
+    // TODO: solve re-rendering issue with values of past story
+    if (storyId && stories) {
+      const resumedStory = stories.find(e => {
+        return e.uid.toString() === storyId;
+      });
+      if (resumedStory) {
+        playerClass = resumedStory.origin.class;
+        name = resumedStory.origin.name;
+        location = resumedStory.origin.location;
+      }
+    }
 
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <MainTitle style={styles.img} />
+        <Image source={require("../assets/title.png")} style={styles.img} />
         <View>
           <TouchableOpacity style={styles.startButton} onPress={this.onStart}>
             <Text style={styles.text}>> Start an Adventure</Text>
           </TouchableOpacity>
         </View>
         {mainStore.storyId ? (
-          <TouchableOpacity style={styles.startButton} onPress={this.onResume}>
-            <Text style={styles.text}>Resume your adventure with</Text>
+          <TouchableOpacity
+            style={[styles.startButton, styles.resumeButton]}
+            onPress={this.onResume}
+          >
+            <View style={styles.resumeView}>
+              <Image
+                style={{ width: 70, height: 70 }}
+                source={PORTRAITS.find(c => c.value === playerClass).portrait}
+              />
+              <Text style={[styles.text, { paddingLeft: 30, width: "80%" }]}>
+                Resume your adventure with{" "}
+                <Text style={styles.bold}>{name}</Text> the{" "}
+                <Text style={styles.bold}>{playerClass}</Text> from{" "}
+                <Text style={styles.bold}>{location}</Text>.
+              </Text>
+            </View>
           </TouchableOpacity>
         ) : null}
       </KeyboardAvoidingView>
