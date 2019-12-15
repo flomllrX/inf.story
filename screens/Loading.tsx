7f9b96fcc5b063d4dc3a20046960885caeaaa285
Florian Mueller <contact@flomllr.com>
2019-12-15 17:43:19 +0100

    Add text to initial loading screen

--- screens/Loading.tsx
+++ screens/Loading.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
+import { colors, fonts } from "../theme";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  text: {
+    color: colors.defaultText,
+    fontFamily: fonts.regular
   }
 });
 
-const Loading: React.SFC<{}> = props => <View style={styles.container}></View>;
+const Loading: React.SFC<{}> = props => (
+  <View style={styles.container}>
+    <Text>Loading...</Text>
+  </View>
+);
 
 Loading.defaultProps = {};
 
